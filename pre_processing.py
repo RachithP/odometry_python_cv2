684bc4067f20f2336f8a0039cccdf10021488138
RachithP <rachithprakash@gmail.com>
2019-04-30 15:50:42 -0400

    made changes to cv2.imread(image) --> cv2.imread(image, -1)
    Signed-off-by: RachithP <rachithprakash@gmail.com>

--- pre_processing.py
+++ pre_processing.py
@@ -1,52 +1,36 @@
 #!/usr/bin/env python2
 import cv2
 import numpy as np
-import ReadCameraModel
+from ReadCameraModel import ReadCameraModel
 import glob
+from UndistortImage import UndistortImage
 
 __author__ = 'rachith'
 
 
-
-def preProcessData(path_to_model):
+def preProcessData(path_to_model, path_to_images):
 	'''
-	In this function, we pre-process the data
-		1. read camera parameters from the file,
-		2. Undistort the image using the previously obtained calibration matrix.
+	Here, we undistort the dataset images
+	:param path_to_model:
+	:param path_to_images:
 	:return:
 	'''
 
 	# Read camera parameters
 	fx, fy, cx, cy, G_camera_image, LUT = ReadCameraModel(path_to_model)
 
-	# iterate through each image, convert to BGR, undistort(function takes BGR as input)
-	# Since there are a ton of images, it would be easier to read, write back the undistorted image rather than storing all the images in an array
-
-
-def convert2BGR(path_to_images):
-	'''
-	In this function, we read images and convert the Bayer format image to BGR
-	:param path_to_images:
-	:return:
-	'''
-
+	# iterate through each image, convert to RGB, undistort(function takes all channels in input)
 	images = glob.glob(path_to_images+"/*.png")
 	images.sort()
 	for cnt, image in enumerate(images):
-		frame = cv2.imread(images[0],0)
-		frame_BGR = cv2.cvtColor(frame, cv2.COLOR_BayerGR2BGR)
-		cv2.imshow('image', frame_BGR)
-		cv2.waitKey(0)
-		quit()
-		frame_BGR = cv2.cvtColor(frame, cv2.COLOR_BayerGR2BGR)
-		cv2.imwrite(path_to_images+"/../undistort/frame/"+cnt+".png", frame_BGR)
-
+		frame = cv2.imread(image, -1)
+		frame_RGB = cv2.cvtColor(frame, cv2.COLOR_BayerGR2BGR)
+		undistorted_image = UndistortImage(frame_RGB, LUT)
+		cv2.imwrite(path_to_images + "/../undistort/frame" + str(cnt) + ".png", undistorted_image)
 
 def main():
+	preProcessData(path_to_model='./model', path_to_images='./stereo/centre')
 
-	convert2BGR(path_to_images='./stereo/centre')
-
-	# preProcessData(path_to_model='./model')
 
 if __name__ == "__main__":
 	main()
