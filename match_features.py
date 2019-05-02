ef53c5d9a41fb99249b9f08f58e180bf74d71c1d
RachithP <rachithprakash@gmail.com>
2019-05-02 12:57:34 -0400

    Mofidying pre-processing.py to take inputs (path). Added Undistort.py to this repo.

--- match_features.py
+++ match_features.py
@@ -17,79 +17,90 @@ from ReadCameraModel import ReadCameraModel
 '''
 get matching pixel coordinates
 '''
-def getPixelCoordinates(kp1,kp2,matches):
+
+
+def getPixelCoordinates(kp1, kp2, matches):
 	# Initialize lists
 	list_kp1 = []
 	list_kp2 = []
 
 	# For each match...
 	for mat in matches:
+		# Get the matching keypoints for each of the images
+		img1_idx = mat.queryIdx
+		img2_idx = mat.trainIdx
 
-	    # Get the matching keypoints for each of the images
-	    img1_idx = mat.queryIdx
-	    img2_idx = mat.trainIdx
+		# x - columns
+		# y - rows
+		# Get the coordinates
+		(x1, y1) = kp1[img1_idx].pt
+		(x2, y2) = kp2[img2_idx].pt
 
-	    # x - columns
-	    # y - rows
-	    # Get the coordinates
-	    (x1,y1) = kp1[img1_idx].pt
-	    (x2,y2) = kp2[img2_idx].pt
+		# Append to each list
+		list_kp1.append((x1, y1))
+		list_kp2.append((x2, y2))
+	return list_kp1, list_kp2
 
-	    # Append to each list
-	    list_kp1.append((x1, y1))
-	    list_kp2.append((x2, y2))
-	return list_kp1,list_kp2
 
-def extractMatchFeatures(image1,image2):
+def extractMatchFeatures(image1, image2):
 	# Initiate SIFT detector
 	orb = cv2.ORB_create()
 
 	# find the keypoints and descriptors with SIFT
-	kp1, des1 = orb.detectAndCompute(image1,None)
-	kp2, des2 = orb.detectAndCompute(image2,None)
+	kp1, des1 = orb.detectAndCompute(image1, None)
+	kp2, des2 = orb.detectAndCompute(image2, None)
 
 	# create BFMatcher object
 	bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
 	# Match descriptors.
-	matches = bf.match(des1,des2)
+	matches = bf.match(des1, des2)
 
 	# Sort them in the order of their distance.
-	matches = sorted(matches, key = lambda x:x.distance)
+	matches = sorted(matches, key=lambda x: x.distance)
 
-	pixelsImg1,pixelsImg2 = getPixelCoordinates(kp1,kp2,matches)
+	pixelsImg1, pixelsImg2 = getPixelCoordinates(kp1, kp2, matches)
 
-	return pixelsImg1,pixelsImg2
+	# img1 = cv2.drawKeypoints(image1, kp1, image1, color=(0, 255, 0), flags=0)
+	# img2 = cv2.drawKeypoints(image2, kp2, image2, color=(255, 0, 0), flags=0)
+	#
+	# cv2.imshow('image1', img1)
+	# cv2.imshow('image2', img2)
+	# cv2.waitKey(0)
+
+	return pixelsImg1, pixelsImg2
+
 
 def extractImages(path):
 	# Read and store all images in the input folder
-	filenames = glob.glob(path+"/*.png")
+	filenames = glob.glob(path + "/*.png")
 	filenames.sort()
 	return filenames
 
-#Get camera model info
-fx,fy,cx,cy,G_camera_image,LUT = ReadCameraModel('./model')
+
+# Get camera model info
+fx, fy, cx, cy, G_camera_image, LUT = ReadCameraModel('./model')
 
 path = './stereo/centre'
 filenames = extractImages(path)
-#Removing first 30 images because it is too bright
+# Removing first 30 images because it is too bright
 del filenames[:30]
 rgbImages = []
 
 counter = 0
 for filename in filenames:
-	image = cv2.imread(filename,0)
+	image = cv2.imread(filename, 0)
 	convertedImage = cv2.cvtColor(image, cv2.COLOR_BAYER_GR2RGB)
 	# convertedImage = cv2.cvtColor(convertedImage, cv2.COLOR_RGB2GRAY)
 	rgbImages.append(convertedImage)
 	counter += 1
-	if counter >100:
+	if counter > 100:
 		break
 # cv2.imshow('image',rgbImages[0])
 # cv2.imshow('image',rgbImages[1])
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
-pixelsImg1,pixelsImg2 = extractMatchFeatures(rgbImages[0],rgbImages[1])
+pixelsImg1, pixelsImg2 = extractMatchFeatures(rgbImages[0], rgbImages[1])
 
-print pixelsImg1,pixelsImg2
+print pixelsImg1, pixelsImg2
